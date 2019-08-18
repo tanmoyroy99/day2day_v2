154db4f065b80910ea42a263ecb69a241af87a88
Tanmoy Roy <tanmoyroy99@gmail.com>
2019-08-18 11:51:38 +0530

    update notepad populated and update

--- src/app/layout/notepad/notepad.component.ts
+++ src/app/layout/notepad/notepad.component.ts
@@ -13,6 +13,7 @@ import {EnvironmentService} from '../../services/environment.service';
   styleUrls: ['./notepad.component.scss']
 })
 export class NotepadComponent implements OnInit {
+  action_url = 'https://daytoday27081989.herokuapp.com/api/notepad/'
   notepadDetails: FormGroup;
   constructor(
       private router: Router,
@@ -25,13 +26,56 @@ export class NotepadComponent implements OnInit {
 
   ngOnInit() {
     this.setNotepadForm();
+    this.dataPopulated();
   }
 
   setNotepadForm(){
-  this.notepadDetails= new FormGroup({
-    notepadInfo: new FormControl('')
-  })
-
+    this.notepadDetails= new FormGroup({
+      notepadInfo: new FormControl('')
+    })
   }
+  dataPopulated(){
+    this.environment.loaderdata = 'active';
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('x-auth-token', localStorage.getItem('authData'));
+    this.http.get(this.action_url+'view/',{ headers: headers }).subscribe(
+      res =>{
+        let info= res.json();
+        this.notepadDetails.controls.notepadInfo.setValue(info[0].notepad);
+      }
+    );
+    this.environment.loaderdata = 'inactive';
+  }
+
+  submitNotepadForm(){
+    this.environment.loaderdata = 'active';
+      let array_daya: any[];
+      array_daya = [{
+        'name': 'NotePad',
+        'notepadInfo':this.notepadDetails.value.notepadInfo
+      }];
+    const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('x-auth-token', localStorage.getItem('authData'));
+
+      this.http.post(this.action_url+'add/',array_daya[0],{ headers: headers }).subscribe(
+        res =>{ console.log(res);
+          this.environment.loaderdata = 'inactive'; 
+          if(res.status == 200){
+            this.snackBar.open('Data Saved Sucessfully...', 'OK', { duration: 10000,});
+          }
+          this.environment.loaderdata = 'inactive';
+        },
+        error=>{
+          this.environment.loaderdata = 'inactive'; 
+          this.snackBar.open(error._body, 'OK', {
+            duration: 10000,
+          });
+          this.environment.loaderdata = 'inactive';
+        });
+  }
+
+
 
 }
